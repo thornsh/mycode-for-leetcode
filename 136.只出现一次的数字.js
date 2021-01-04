2383dd1b07873beb7fe3f502c54238754272dac7
兰世豪 <1183643226@qq.com>
2021-01-04 17:22:47 +0800

    update136 使用高阶函数

--- 136.只出现一次的数字.js
+++ 136.只出现一次的数字.js
@@ -10,7 +10,21 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
+  var current_item;
+  nums.map((item,index)=>{
+    var new_nums = nums.filter((i,j)=>{
+      return j != index;
+    })
+    if(new_nums.indexOf(item)===-1){
+      current_item = item;
+    }
+  })
 
+  return current_item;
 };
 // @lc code=end
 
+
+// @after-stub-for-debug-begin
+module.exports = singleNumber;
+// @after-stub-for-debug-end
